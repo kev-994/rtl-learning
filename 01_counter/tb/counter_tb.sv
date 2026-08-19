7f964af9f5de28af3da37ab6df2d43afdf8e413b
Kevin Magombe <kevinmagombe@outlook.com>
2026-08-19 12:07:35 +0100

    procedural assertion

--- 01_counter/tb/counter_tb.sv
+++ 01_counter/tb/counter_tb.sv
@@ -19,15 +19,14 @@ module counter_tb;
 
     initial begin
         expected = 0;
+        
         @(negedge reset);
+        
         repeat (10) begin
             @(posedge clk);
-            if (expected == count)
-                $display("t=%0t PASS: expected=%0d actual=%0d",
-                        $time, expected, count);
-            else
-                $display("t=%0t FAIL: expected=%0d actual=%0d",
-                        $time, expected, count);
+            
+            assert (count == expected)
+                else $error("Count does not match expected");
 
             ++expected;
             
