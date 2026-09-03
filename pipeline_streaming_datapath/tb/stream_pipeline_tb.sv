ee9773d410d9227473c3341ffadc6ed84673f143
Kevin Magombe <kevinmagombe@outlook.com>
2026-09-03 19:44:03 +0100

    A few tests

---
+++ pipeline_streaming_datapath/tb/stream_pipeline_tb.sv
@@ -0,0 +1,164 @@
+`timescale 1ns/1ps
+module stream_pipeline_tb;
+
+    parameter DATA_WIDTH = 8;
+
+    logic clk;
+    logic reset;
+
+    logic [DATA_WIDTH-1:0] in_data;
+    logic                  in_valid;
+    logic                  in_ready;
+
+    logic [DATA_WIDTH-1:0] out_data;
+    logic                  out_valid;
+    logic                  out_ready;
+
+    stream_pipeline #(
+        .DATA_WIDTH(DATA_WIDTH)
+    ) dut (
+        .clk       (clk),
+        .reset     (reset),
+        .in_data   (in_data),
+        .in_valid  (in_valid),
+        .in_ready  (in_ready),
+        .out_data  (out_data),
+        .out_valid (out_valid),
+        .out_ready (out_ready)
+    );
+
+    task automatic reset_sp();
+        reset = 1;
+        @(posedge clk);
+        #1;
+        reset = 0;
+    endtask
+
+    task automatic test_reset();
+        reset_sp();
+        
+        assert (in_ready) else $error("in_ready should be asserted after reset.");
+        assert (!out_valid) else $error("out_valid asserted after reset.");
+        assert (out_data == 0) else $error("data in buffer should be reset to 0 after reset.");
+    endtask
+    
+    task automatic test_transaction_enters(input logic [DATA_WIDTH-1:0] test_data);
+        reset_sp();
+
+        in_data = test_data;
+        in_valid = 1;
+
+        assert (in_ready) else $error("Buffer not ready to accept transaction.");
+        
+        @(posedge clk);
+        #1;
+
+        in_valid = 0;
+
+        assert (out_valid) else $error("out_valid not asserted when buffer is full.");
+        assert (out_data == test_data) else $error("out_data has unexpected value.");
+        assert (!in_ready) else $error("Buffer should be full after transaction.");
+    endtask
+
+    task automatic test_transaction_leaves(input logic [DATA_WIDTH-1:0] test_data);
+        reset_sp();
+
+        in_data = test_data;
+        in_valid = 1;
+
+        @(posedge clk);
+        #1;
+
+        in_valid = 0;
+
+        out_ready = 1;
+
+        @(posedge clk);
+        #1;
+
+        out_ready = 0;
+        
+        assert (!out_valid) else $error("out_valid asserted though buffer should be empty.");
+        assert (in_ready) else $error("in_ready not asserted though buffer is empty");
+    endtask
+
+    task automatic test_simultaneous_transfer(input logic [DATA_WIDTH-1:0] test_data1, input logic [DATA_WIDTH-1:0] test_data2);
+        reset_sp();
+
+        in_data = test_data1;
+        in_valid = 1;
+
+        @(posedge clk);
+        #1;
+
+        in_valid = 0;
+        
+        ///// other transaction
+        
+        out_ready = 1;
+        in_data = test_data2;
+        in_valid = 1;
+
+        #1; // give time for in_ready to evaluate the combinational logic
+
+        // $display("in_ready=%b out_ready=%b out_valid=%b in_valid=%b",
+        //  in_ready, out_ready, out_valid, in_valid);
+
+        assert (out_valid) else $error("out_valid not asserted though currenct transaction is being consumed.");
+        assert (in_ready) else $error("in_ready not asserted though current transaction is being consumed.");
+
+        @(posedge clk);
+        #1;
+
+        in_valid = 0;
+
+        assert (in_ready) else $error("in_ready asserted during continuous streaming.");  // out_ready is still high
+        assert (out_valid) else $error("out_valid not asserted though buffer is full.");
+        assert (out_data == test_data2) else $error("out_data has unexpected value.");
+    endtask
+
+    always #5 clk = ~clk; // time period of 10ns
+
+    initial begin
+        $dumpfile("sim/stream_pipeline.vcd");
+        $dumpvars(0, stream_pipeline_tb);
+        $dumpvars(0, stream_pipeline_tb.dut);
+        
+        clk       = 0;
+        reset     = 1;
+        in_valid  = 0;
+        out_ready = 0;
+
+
+        // test reset
+        test_reset();
+
+
+        // test transaction entering
+        test_transaction_enters(8'hA5);
+
+
+        // off the back of last test, test consumer backpressure
+        out_ready = 0;
+        in_valid  = 1;
+        in_data   = 8'h3C;
+
+        @(posedge clk);
+        #1;
+
+        assert (!in_ready) else $error("in_ready asserted though buffer is full.");
+        assert (out_valid) else $error("out_valid not asserted though buffer is full.");
+        assert (out_data == 8'hA5) else $error("Data entered a full buffer.");
+
+
+        // test transaction leaving
+        test_transaction_leaves(8'hA5);
+
+        
+        // test simultaneous transfer
+        test_simultaneous_transfer(8'hA5, 8'h3C);
+
+        $finish;
+    end
+
+endmodule
