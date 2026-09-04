644e99f0eff74f629502758212997209b4e416da
Kevin Magombe <kevinmagombe@outlook.com>
2026-09-04 13:06:07 +0100

    Arithmetic added

--- pipeline_streaming_datapath/rtl/arithmetic_datapath.sv
+++ pipeline_streaming_datapath/rtl/arithmetic_datapath.sv
@@ -1,20 +1,24 @@
 `timescale 1ns / 1ps
 module arithmetic_datapath #(
-    parameter DATA_WIDTH = 32
+    parameter DATA_WIDTH = 32,
+    parameter logic [DATA_WIDTH-1:0] A = 2,
+    parameter logic [DATA_WIDTH-1:0] B = 3,
+    parameter logic [DATA_WIDTH-1:0] C = 4
 )(
-    input  logic                  clk,
-    input  logic                  reset,
+    input  logic                    clk,
+    input  logic                    reset,
 
-    input  logic [DATA_WIDTH-1:0] in_data,
-    input  logic                  in_valid,
-    output logic                  in_ready,
+    input  logic [DATA_WIDTH-1:0]   in_data,
+    input  logic                    in_valid,
+    output logic                    in_ready,
 
-    output logic [DATA_WIDTH-1:0] out_data,
-    output logic                  out_valid,
-    input  logic                  out_ready
+    output logic [2*DATA_WIDTH+1:0] out_data,
+    output logic                    out_valid,
+    input  logic                    out_ready
 );
     logic valid0, ready0, valid1, ready1;
-    logic [DATA_WIDTH-1:0] data_reg0, data_reg1;
+    logic [DATA_WIDTH:0] data_reg0;
+    logic [2*DATA_WIDTH:0] data_reg1;
     
     assign in_ready = !valid0 || ready0;
     assign ready0   = !valid1 || ready1;
@@ -30,7 +34,7 @@ module arithmetic_datapath #(
         else begin
             // stage 0
             if (in_ready && in_valid) begin 
-                data_reg0 <= in_data;
+                data_reg0 <= {1'b0, in_data} + {1'b0, A}; // make operands wider before operation
                 valid0    <= 1'b1;
             end
             else if (ready0 && valid0) begin
@@ -39,7 +43,7 @@ module arithmetic_datapath #(
 
             // stage 1
             if (ready0 && valid0) begin 
-                data_reg1 <= data_reg0;
+                data_reg1 <= {{DATA_WIDTH{1'b0}}, data_reg0} * B; // explicitly extend one operand to 2W+1 bits
                 valid1    <= 1'b1;
             end
             else if (valid1 && ready1) begin
@@ -48,7 +52,7 @@ module arithmetic_datapath #(
 
             // stage 2
             if (ready1 && valid1) begin 
-                out_data <= data_reg1;
+                out_data <= {1'b0, data_reg1} + {{(DATA_WIDTH+1){1'b0}}, C}; // make operands wider before operation
                 out_valid    <= 1'b1;
             end
             else if (out_valid && out_ready) begin
@@ -56,7 +60,5 @@ module arithmetic_datapath #(
             end
         end
     end
-
-
 
 endmodule
